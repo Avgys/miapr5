e8062e27ebc4e6bd5672f22f46a71b68ad485a19
ilya kosykh <ilyuha.prosto.97@gmail.com>
2022-04-06 23:50:44 +0300

    Added main logic

---
+++ Function.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+
+namespace PotentialMethod
+{
+    public class Function
+    {
+        private double XKoeff { get; set; }
+        private double YKoeff { get; set; }
+        private double XyKoeff { get; set; }
+        private double FreeKoeff { get; set; }
+        public Function(double xKoeff, double yKoeff, double xyKoeff, double freeKoeff)
+        {
+            XKoeff = xKoeff;
+            YKoeff = yKoeff;
+            XyKoeff = xyKoeff;
+            FreeKoeff = freeKoeff;
+        }
+
+        public double GetValue(Point point)
+        {
+            return FreeKoeff + XKoeff * point.X + YKoeff * point.Y + XyKoeff * point.X * point.Y;
+        }
+
+        public double GetY(double x)
+        {
+            return -(XKoeff * x + FreeKoeff) / (XyKoeff * x + YKoeff);
+        }
+
+        public override string ToString()
+        {
+            if (XyKoeff != 0)
+            {
+                return
+                    $"y=({-XKoeff}*x{(-FreeKoeff < 0 ? "" : "+")}{-FreeKoeff})/({XyKoeff}*x{(YKoeff < 0 ? "" : "+")}{YKoeff})";
+            }
+            if (YKoeff != 0)
+            {
+                return
+                    $"y={-(double)XKoeff / YKoeff}*x{(-(double)FreeKoeff / YKoeff < 0 ? "" : "+")}{-(double)FreeKoeff / YKoeff}";
+            }
+            return $"x={-(double)FreeKoeff / XKoeff}";
+        }
+
+        public static Function operator +(Function first, Function second)
+        {
+            return new Function(first.XKoeff + second.XKoeff, first.YKoeff + second.YKoeff,
+                first.XyKoeff + second.XyKoeff, first.FreeKoeff + second.FreeKoeff);
+        }
+
+        public static Function operator *(int koeff, Function function)
+        {
+            return new Function(koeff * function.XKoeff, koeff * function.YKoeff, koeff * function.XyKoeff,
+                koeff * function.FreeKoeff);
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
+﻿using PotentialMethod;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace miapr5
 {
@@ -20,9 +14,119 @@ namespace miapr5
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Graph _graph;
+        private Function _desiciveFunction;
+        private readonly List<Point>[] _points = new List<Point>[2];
+        private const int Step = 21;
+
         public MainWindow()
         {
             InitializeComponent();
+            ClearPoints();
+        }
+
+        private void ClearPoints()
+        {
+            _points[0] = new List<Point>();
+            _points[1] = new List<Point>();
+        }
+
+        private void TextCheck(object sender, TextCompositionEventArgs e)
+        {
+            var regex = new Regex("(^-?[0-9]+$)|(^-?$)");
+            if (e.Text == " ") e.Handled = true;
+            if (!regex.IsMatch($"{(sender as TextBox)?.Text}{e.Text}")) e.Handled = true;
+        }
+
+        private void ButtonTrain_OnClick(object sender, RoutedEventArgs e)
+        {
+            ClearPoints();
+            var potential = new Potential();
+            var teaching = new Point[2][];
+
+            teaching[0] = new Point[2];
+            teaching[1] = new Point[2];
+            try
+            {
+                teaching[0][0] = new Point(int.Parse(TextBoxO1X.Text), int.Parse(TextBoxO1Y.Text));
+                teaching[0][1] = new Point(int.Parse(TextBoxO2X.Text), int.Parse(TextBoxO2Y.Text));
+                teaching[1][0] = new Point(int.Parse(TextBoxO3X.Text), int.Parse(TextBoxO3Y.Text));
+                teaching[1][1] = new Point(int.Parse(TextBoxO4X.Text), int.Parse(TextBoxO4Y.Text));
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Ошибка заполнения полей");
+                return;
+            }
+
+            _points[0].Add(teaching[0][0]);
+            _points[0].Add(teaching[0][1]);
+            _points[1].Add(teaching[1][0]);
+            _points[1].Add(teaching[1][1]);
+
+
+            _desiciveFunction = potential.GetFunction(teaching);
+            TextBoxFunction.Text = "";
+            if (!potential.Warning)
+            {
+                TextBoxFunction.Text = _desiciveFunction.ToString();
+                ButtonClassify.IsEnabled = true;
+                _graph = new Graph(_desiciveFunction, Canvas.ActualWidth, Canvas.ActualHeight);
+            }
+            else
+            {
+                MessageBox.Show("Невозможно построить разделяющую функцию");
+                _graph = new Graph(Canvas.ActualWidth, Canvas.ActualHeight);
+                ButtonClassify.IsEnabled = false;
+            }
+
+            Canvas.Source = new DrawingImage(_graph.DrawingGroup);
+            for (var i = 0; i < 2; i++)
+            {
+                for (var j = 0; j < 2; j++)
+                {
+                    _graph.AddPoint(teaching[i][j], i == 0);
+                }
+            }
+        }
+
+        private void ButtonClassify_OnClick(object sender, RoutedEventArgs e)
+        {
+            Point testPoint;
+            try
+            {
+                testPoint = new Point(int.Parse(TextBoxOX.Text), int.Parse(TextBoxOY.Text));
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Заполните X и Y точки для классификации");
+                return;
+            }
+
+            var classNumber = _desiciveFunction.GetValue(testPoint) >= 0 ? 0 : 1;
+            _points[classNumber].Add(testPoint);
+            MessageBox.Show($"Класс {classNumber + 1}");
+            _graph.AddPoint(testPoint, classNumber == 0);
+        }
+
+        private void Move_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (DrawToolTip(e, _points[0], 1)) return;
+            if (DrawToolTip(e, _points[1], 2)) return;
+        }
+
+        private bool DrawToolTip(MouseEventArgs e, IEnumerable<Point> list, int classNumber)
+        {
+            foreach (var point in list)
+            {
+                var position = e.GetPosition(Canvas);
+                if (!(Math.Abs(point.X * Step + Canvas.ActualWidth / 2 - position.X) < 10) ||
+                    !(Math.Abs(Canvas.ActualHeight / 2 - point.Y * Step - position.Y) < 10)) continue;
+                Canvas.ToolTip = $"Класс:{classNumber}{Environment.NewLine}({point.X};{point.Y})";
+                return true;
+            }
+
+            return false;
         }
     }
 }
